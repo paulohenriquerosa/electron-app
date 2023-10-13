592925e836695ba8c717e76576bca9a96ae1ee3e
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2023-10-13 19:12:20 -0300

    init: fundation and setup

---
+++ electron.vite.config.ts
@@ -0,0 +1,36 @@
+import { resolve } from 'node:path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    publicDir: resolve('assets'),
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+
+    define: {
+      "process.platform": JSON.stringify(process.platform),
+    },
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss({
+            config: './src/renderer/tailwind.config.js',
+          }),
+        ],
+      },
+    },
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+      },
+    },
+    plugins: [react()],
+  },
+})
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import './styles/global.css'
+
+export function App(): JSX.Element {
+  return (
+    <div className=" flex h-screen w-screen bg-rotion-900 text-rotion-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
+  )
+}
